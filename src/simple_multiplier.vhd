b39430491cdb7a527d1338037e38a29afe0954f2
Job Tijhuis <jobstijhuis@gmail.com>
2022-11-02 15:02:05 +0100

    IO names are different, fixed now

--- src/simple_multiplier.vhd
+++ src/simple_multiplier.vhd
@@ -4,16 +4,24 @@ use IEEE.numeric_std.all;
 
 entity simple_multiplier is
     port (
-        a_in    : in std_logic_vector(2 downto 0);
-        b_in    : in std_logic_vector(2 downto 0);
-        c_out   : out std_logic_vector(5 downto 0)
+        io_in   : in std_logic_vector(7 downto 0);
+        io_out  : out std_logic_vector(7 downto 0)
     );
 end entity simple_multiplier;
 
 architecture rtl of simple_multiplier is
     
+    signal a_in  : std_logic_vector(2 downto 0);
+    signal b_in  : std_logic_vector(2 downto 0);
+    signal c_out : std_logic_vector(5 downto 0);
+
 begin
-    
+
+    a_in <= io_in(4 downto 2);
+    b_in <= io_in(7 downto 5);
+
     c_out <= std_logic_vector(unsigned(a_in) * unsigned(b_in));
+
+    io_out(5 downto 0) <= c_out;
     
 end architecture rtl;
